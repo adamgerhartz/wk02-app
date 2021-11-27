2816f61c25748efbbc5eb2c7164eda3c10656f16
Adam Gerhartz <adamrussellgerhartz@gmail.com>
2021-11-26 20:59:55 -0700

    Add connection to the mongo database

--- server.js
+++ server.js
@@ -5,12 +5,24 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
 
 // import the routing file to handle the default (index) route
 const index = require('./server/routes/app');
 const messageRoutes = require('./server/routes/messages');
 const contactRoutes = require('./server/routes/contacts');
 const documentRoutes = require('./server/routes/documents');
+
+// establish a connection to the mongo database
+mongoose.connect('mongodb://localhost:27017/cms',
+  { useNewUrlParser: true }, (err, res) => {
+    if (err) {
+      console.log(`Connection failed: ${err}`);
+    } else {
+      console.log("Connected to database!");
+    }
+  }
+);
 
 const app = express(); // create an instance of express
 
